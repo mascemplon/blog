c7f79eb202006520b2b347af820942cb59122580
mascemplon <77086461+mascemplon@users.noreply.github.com>
2021-01-09 08:58:46 +0700

    Add files via upload

---
+++ komik/js/sitemap.js
@@ -0,0 +1,33 @@
+var numposts = 10;
+var standardstyling = true;
+
+function startpost(json){
+  for (var i = 0; i < numposts; i++){
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    
+    if (i == json.feed.entry.length) break;
+    
+    for (var k = 0; k < entry.link.length; k++){
+      if (entry.link[k].rel == 'alternate'){
+        posturl = entry.link[k].href;break;
+      }
+    }
+        
+    posttitle = posttitle.link(posturl);
+    if (standardstyling) document.write('<li>');
+    
+    document.write(posttitle);
+  }
+  
+  if (standardstyling) document.write('</li>');
+}
+
+function finished(){}
+
+
+
+
+
+
